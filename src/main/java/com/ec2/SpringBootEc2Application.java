8a31efe997ca79d7a8d0e50115c5138b6dbd4362
Carlos Zuluaga <andres.zuluaga6@udea.edu.co>
2025-09-04 08:33:14 -0500

    ec2 conencted with ubuntu

---
+++ src/main/java/com/ec2/SpringBootEc2Application.java
@@ -0,0 +1,27 @@
+package com.ec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class SpringBootEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEc2Application.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String HelloWorld(){
+		return "Hello World- Get";
+	}
+	@PostMapping("/hello")
+	public String SayHelloPost(){
+		return "Hello World- Post";
+	}
+
+}
